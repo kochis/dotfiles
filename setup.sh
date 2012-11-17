d7a0cc692210ab7132b7533af4012bbb06561c2e
Craig Kochis <craig@substantial.com>
2012-11-16 23:40:07 -0800

    fix git configs

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo 'Symlinking dotfiles'
 ln -s ~/.dotfiles/ackrc ~/.ackrc
 ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/gitconfig ~/.gitignore_global
+ln -s ~/.dotfiles/gitignore_global ~/.gitignore_global
 ln -s ~/.dotfiles/gemrc ~/.gemrc
 
 echo '!!! Dont forget to update .gitconfig !!!'
