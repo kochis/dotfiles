323fab04858eb24a50e58f35fa2292a2dadd69d7
Craig Kochis <craig@substantial.com>
2012-11-29 00:18:47 -0800

    add tmux.conf

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/gitignore_global ~/.gitignore_global
 ln -s ~/.dotfiles/gemrc ~/.gemrc
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 
 echo '!!! Dont forget to update .gitconfig !!!'
